9597fd06b3b81da7e08e1081ac1f8d5ffbac7488
YYZA <92071754+YYZA@users.noreply.github.com>
2021-11-01 22:33:15 +0900

    login page

--- src/views/login_pages.py
+++ src/views/login_pages.py
@@ -2,6 +2,8 @@ from flask import Blueprint, render_template
 
 login_pages = Blueprint('login_pages', __name__, url_prefix="/login")
 
+
 @login_pages.route("/")
 def login():
     return render_template("login.html")
+
