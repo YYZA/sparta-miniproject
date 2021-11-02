e343fb49238af34c49f0d58ca98184fab1ea3ad7
YYZA <92071754+YYZA@users.noreply.github.com>
2021-11-02 14:28:58 +0900

    join page

--- src/static/js/join.js
+++ src/static/js/join.js
@@ -10,36 +10,50 @@ window.addEventListener('load', () => {
     });
 }, false);
 
+const USER_PW = document.getElementById('pw')
+const PW_CHECK = document.getElementById('check')
 function check_pw() {
-    let pw = document.getElementById('pw').value;
+    let pw = USER_PW.value;
     let SC = ["!", "@", "#", "$", "%"];
     let check_SC = 0;
 
     if (pw.length < 6 || pw.length > 16) {
         window.alert('비밀번호는 6글자 이상, 16글자 이하만 이용 가능합니다.');
-        document.getElementById('pw').value = '';
+        USER_PW.value = '';
     }
     for (let i = 0; i < SC.length; i++) {
         if (pw.indexOf(SC[i]) != -1) {
             check_SC = 1;
         }
     }
-    if (check_SC == 0) {
+    if (check_SC === 0) {
         window.alert('!,@,#,$,% 의 특수문자가 들어가 있지 않습니다.')
-        document.getElementById('pw').value = '';
+        USER_PW.value = '';
     }
-    if (document.getElementById('pw').value != '' && document.getElementById('pw2').value != '') {
-        if (document.getElementById('pw').value == document.getElementById('pw2').value) {
-            document.getElementById('check').innerHTML = '비밀번호가 일치합니다.'
-            document.getElementById('check').style.color = 'blue';
+    if (USER_PW.value !== '' && USER_PW.value !== '') {
+        if (USER_PW.value == USER_PW.value) {
+            PW_CHECK.innerHTML = '비밀번호가 일치합니다.'
+            PW_CHECK.style.color = 'blue';
         } else {
-            document.getElementById('check').innerHTML = '비밀번호가 일치하지 않습니다.';
-            document.getElementById('check').style.color = 'red';
+            PW_CHECK.innerHTML = '비밀번호가 일치하지 않습니다.';
+            PW_CHECK.style.color = 'red';
         }
     }
 }
 
-function goLoginForm() {
-            window.location="/login";
-        }
 
+function sendDB() {
+                let email = $('#email').val()
+                let nickname = $('#nickname').val()
+                let name = $('#name').val()
+                let pw = $('#pw').val()
+
+                $.ajax({
+                    type: "POST",
+                    url: "/join",
+                    data: {email, nickname, name, pw },
+                    success: function (response) { // 성공하면
+                        window.location("/login")
+                        }
+                     })
+                    }
