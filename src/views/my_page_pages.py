104b5ee819b8246fa4e21fd167650b5f5cedd7aa
seungbin-park <tmdqls0212@naver.com>
2021-11-03 18:05:21 +0900

    mypage.html

--- src/views/my_page_pages.py
+++ src/views/my_page_pages.py
@@ -6,22 +6,9 @@ my_page_pages = Blueprint('my_page_pages', __name__, url_prefix="/mypage")
 def my_page():
     return render_template("my_page.html")
 
-@my_page_pages.route("/upload", methods=["POST"])
-def write_box():
-    file_receive=request.form['file_give']
-    name_receive = request.form['name_give']
-    age_receive = request.form['age_give']
-    breed_receive = request.form['breed_give']
-    intro_receive = request.form['name_give']
 
-    doc={
-        'file':'file_receive',
-        'name':'name_receive',
-        'age': 'age_receive',
-        'breed': 'breed_receive',
-        'intro': 'intro_receive',
 
-    }
+
 
 
 
